ac1b202fc2a826cb13b6d56014aabc9643d1ceb6
Elaad Applebaum <elaad.applebaum@gmail.com>
2019-01-26 11:39:47 -0500

    Change boxes to menus

--- translate.py
+++ translate.py
@@ -13,6 +13,7 @@ class Application(tk.Frame):
         self.textcolor2 = '#d2c6a1'
         self.pack()
         self.master.configure(bg=self.bgcolor)
+        self.lang_list = ['en', 'es', 'it', 'fr', 'he', 'ar']
         self.create_widgets()
 
     def create_widgets(self):
@@ -23,60 +24,71 @@ class Application(tk.Frame):
         self.topframe.configure(bg=self.bgcolor)
         self.botframe.configure(bg=self.bgcolor)
         self.master.bind('<Return>', self.translate_word)
-
-        self.l1 = tk.Label(self.topframe, text='Word to translate:',
+        
+        self.src = tk.StringVar(self.master) #source language
+        self.src.set(self.lang_list[0])
+        self.srcmenu = tk.OptionMenu(self.topframe, self.src, *self.lang_list)
+        self.srcmenu.configure(bg=self.textcolor2, activebackground=self.textcolor1,
                 font='Arial 18')
-        self.l1.grid(row=0, column=0, ipadx=50)
+        self.srcmenu['menu'].configure(bg=self.textcolor1, font='Arial 18')
+        self.srcmenu.grid(row=0, column=0, ipadx=50, padx=20)
+        self.l1 = tk.Label(self.topframe, text='word to translate:',
+                font='Arial 18')
+        self.l1.grid(row=0, column=1)#, ipadx=50)
         self.entry = tk.Entry(self.topframe, font='Courier 18')
-        self.entry.grid(row=0, column=1)
+        self.entry.grid(row=0, column=2)
         self.entry.configure(bg=self.textcolor1)
         self.l1.configure(bg=self.bgcolor)
 
         self.b1 = tk.Button(self.topframe, text='Translate!', command=self.translate_word,
                  font='Arial 18')
-        self.b1.grid(row=0, column=2, padx=10)
+        self.b1.grid(row=0, column=3, padx=10)
         self.b1.configure(bg=self.bgcolor)
 
-        self.lang_check = {}
-        self.make_checkbox(self.botframe, 'es')
-        self.make_checkbox(self.botframe, 'it')
-        self.make_checkbox(self.botframe, 'fr')
-        self.make_checkbox(self.botframe, 'he')
+        self.lang_menu = []
+        for lang in self.lang_list[1:5]:
+            self.make_menu(self.botframe, lang)
+        #self.make_checkbox(self.botframe, 'it')
+        #self.make_checkbox(self.botframe, 'fr')
+        #self.make_checkbox(self.botframe, 'he')
         #for lang in self.lang_check:
         #    self.lang_check[lang][1].pack()
         
-        for i, lang in enumerate(self.lang_check):
+        for i in range(len(self.lang_menu)):
             self.botframe.rowconfigure(i+1, weight=1)
-            self.lang_check[lang][1].configure(bg=self.bgcolor)
+            self.lang_menu[i][1].configure(bg=self.bgcolor)
 
-    def make_checkbox(self, frame, lang):
-        self.lang_check[lang] = [tk.IntVar()]
-        self.lang_check[lang][0].set(1)
-        self.lang_check[lang].append(tk.Checkbutton(frame, text=lang, font='Arial 14', \
-                variable=self.lang_check[lang][0], onvalue=1, offvalue=0, height=5, \
-                width=5))
+    def make_menu(self, frame, lang):
+        self.lang_menu.append([tk.StringVar()])
+        self.lang_menu[-1][0].set(lang)
+        self.lang_menu[-1].append(tk.OptionMenu(frame, self.lang_menu[-1][0], 
+            *self.lang_list))
+        self.lang_menu[-1][1].configure(font='Arial 14', bg=self.textcolor2,
+            activebackground=self.textcolor1)
+        self.lang_menu[-1][1]['menu'].configure(bg=self.textcolor1, font='Arial 14')
 
-        self.lang_check[lang].append(tk.Text(frame, font='Arial 18', width=60,
+        self.lang_menu[-1].append(tk.Text(frame, font='Arial 18', width=60,
             bg=self.textcolor1))
         
-        self.lang_check[lang][1].grid(row=len(self.lang_check), column=0)
-        self.lang_check[lang][2].grid(row=len(self.lang_check), column=1)
+        self.lang_menu[-1][1].grid(row=len(self.lang_menu), column=0)
+        self.lang_menu[-1][2].grid(row=len(self.lang_menu), column=1)
         #self.lang_check[lang][1].pack(side='top')
         #self.lang_check[lang][2].pack(side='right')
 
     def translate_word(self, event=None):
-        translations = [self.tr_obj.translate(self.entry.get(), dest=lang)
-                if self.lang_check[lang][0].get()
+        translations = [self.tr_obj.translate(self.entry.get(), dest=self.lang_menu[i][0].get(), 
+            src=self.src.get())
+                if self.lang_menu[i][0].get()
                 else -1
-                for lang in self.lang_check]
+                for i in range(len(self.lang_menu))]
         
-        for i, lang in enumerate(self.lang_check):
+        for i in range(len(self.lang_menu)):
             if translations[i] != -1:
-                if lang is 'he':
+                if translations[i].dest in ['he', 'ar']:
                     display_text = translations[i].text[::-1]
                 else:
                     display_text = translations[i].text
-                self.lang_check[lang][2].replace('1.0', tk.END, display_text)
+                self.lang_menu[i][2].replace('1.0', tk.END, display_text)
 
 if __name__ == '__main__':
 
